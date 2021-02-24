3c2371b95d8870f2efd534be72158936fb191631
AfonsoAbreu <65315866+AfonsoAbreu@users.noreply.github.com>
2021-02-24 14:24:16 -0300

    [OFF] Added a autodestruct route for convenience

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
+import { getConnection } from "typeorm";
 
 import "./database";
 import router from "./routes";
 
 const app = express();
+
+app.post("/autodestruct", async (req: Request, res: Response) => {
+  //NOOOOOO PERRY, DONT HIT THE BUTTON
+  const entities = await getConnection().entityMetadatas;
+  for (const entity of entities) {
+    const repo = await getConnection().getRepository(entity.name);
+    await repo.clear();
+  }
+  res.send("CURSE YOU, PERRY THE PLATYPUS!!!!").status(200);
+});
 
 app.use(express.json());
 app.use(router);
